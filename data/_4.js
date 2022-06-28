62e42b0a11f67cb8c37b5255c8bbbad8e01e460e
kapanadze <kapan.gio777@gmail.com>
2022-06-28 20:11:19 +0400

    v1
    v1

---
+++ data/_4.js
@@ -0,0 +1,1 @@
+var json__4 = {"type":"FeatureCollection","name":"_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"tipi":null},"geometry":{"type":"Point","coordinates":[44.786491033541857,41.749785004065366]}}]}
